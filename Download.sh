802d910576d25ef00e012586f22b7e19811a8d28
gg <hisopfun@gmail.com>
2021-07-26 21:17:45 +0800

    update script

--- Download.sh
+++ Download.sh
@@ -1,4 +1,11 @@
 #!bin/bash
 
-bash /home/gg/Taifex/Daily_Fut_CSV/Download.sh
-bash /home/gg/Taifex/Daily_Opt_CSV/Download.sh
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+
+
+bash $SCRIPTPATH/Daily_Fut_CSV/Download.sh
+bash $SCRIPTPATH/Daily_Opt_CSV/Download.sh

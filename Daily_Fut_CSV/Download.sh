fe1f30257f3e94d55ceccee8256eac710974a053
gg <hisopfun@gmail.com>
2021-07-26 21:12:21 +0800

    update script

--- Daily_Fut_CSV/Download.sh
+++ Daily_Fut_CSV/Download.sh
@@ -6,31 +6,47 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 echo $SCRIPTPATH
 
+function getFile(){
+	#$1=dir $2=filename $3=url no file
+	echo $2"(Downloading......)";
+	mkdir $1
+	curl $3$2 > $1/$2  
+		
+}
+
 # Time_Sales_Data/
-Filename="Daily_"$(date +"%Y_%m_%d")".zip"
-url="https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV/"$Filename
-echo $url
-mkdir  $SCRIPTPATH/Time_Sales_Data
-curl $url > $SCRIPTPATH/Time_Sales_Data/$Filename
+#Filename="Daily_"$(date +"%Y_%m_%d")".zip"
+#url="https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV/"$Filename
+#echo $url
+#dir=$SCRIPTPATH/Time_Sales_Data
+#mkdir $dir
+#curl $url > $dir/$Filename
+getFile $SCRIPTPATH/Time_Sales_Data "Daily_"$(date +"%Y_%m_%d")".zip" "https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV/"
 
 # Volume_Report_On_Calender_Spread_Order/
-Filename="Daily_"$(date +"%Y_%m_%d")"_B.zip"
-url="https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV_B/"$Filename
-echo $url
-mkdir  $SCRIPTPATH/Volume_Report_On_Calender_Spread_Order
-curl $url > $SCRIPTPATH/Volume_Report_On_Calender_Spread_Order/$Filename
+#Filename="Daily_"$(date +"%Y_%m_%d")"_B.zip"
+#url="https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV_B/"$Filename
+#echo $url
+#dir=$SCRIPTPATH/Volume_Report_On_Calender_Spread_Order
+#mkdir $dir
+#curl $url > $dir/$Filename
+getFile $SCRIPTPATH/Volume_Report_On_Calender_Spread_Order "Daily_"$(date +"%Y_%m_%d")"_B.zip" "https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV_B/"
 
 # Time_Sales_Data_On_Calender_Spread_Orders/
-Filename="Daily_"$(date +"%Y_%m_%d")"_C.zip"
-url="https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV_C/"$Filename
-echo $url
-mkdir  $SCRIPTPATH/Time_Sales_Data_On_Calender_Spread_Orders
-curl $url > $SCRIPTPATH/Time_Sales_Data_On_Calender_Spread_Orders/$Filename
+#Filename="Daily_"$(date +"%Y_%m_%d")"_C.zip"
+#url="https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV_C/"$Filename
+#echo $url
+#dir=$SCRIPTPATH/Time_Sales_Data_On_Calender_Spread_Orders
+#mkdir $dir
+#curl $url > $dir/$Filename
+getFile $SCRIPTPATH/Time_Sales_Data_On_Calender_Spread_Orders "Daily_"$(date +"%Y_%m_%d")"_C.zip" "https://www.taifex.com.tw/file/taifex/Dailydownload/DailydownloadCSV_C/"
 
 # Time_Sales_Data_On_Calender_Spread_Orders/
-Filename="LI_"$(date +"%Y%m%d")".csv"
-url="https://www.taifex.com.tw/file/taifex/Dailydownload/LI/eng/FUT/"$Filename
-echo $url
-mkdir  $SCRIPTPATH/Liquidity_Indicator
-curl $url > $SCRIPTPATH/Liquidity_Indicator/$Filename
+#Filename="LI_"$(date +"%Y%m%d")".csv"
+#url="https://www.taifex.com.tw/file/taifex/Dailydownload/LI/eng/FUT/"$Filename
+#echo $url
+#dir=$SCRIPTPATH/Liquidity_Indicator
+#mkdir $dir
+#curl $url > $dir/$Filename
+getFile $SCRIPTPATH/Liquidity_Indicator "LI_"$(date +"%Y%m%d")".csv" "https://www.taifex.com.tw/file/taifex/Dailydownload/LI/eng/FUT/"
 
--- Daily_Opt_CSV/Download.sh
+++ Daily_Opt_CSV/Download.sh
@@ -6,10 +6,37 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 echo $SCRIPTPATH
 
-# Time_Sales_Data/
-Filename="OptionsDaily_"$(date +"%Y_%m_%d")".zip"
-url="https://www.taifex.com.tw/file/taifex/Dailydownload/OptionsDailydownloadCSV/"$Filename
-echo $url
-mkdir $SCRIPTPATH/Time_Sales_Data
-curl $url > $SCRIPTPATH/Time_Sales_Data/$Filename
+function getFile(){
+	#$1=dir $2=filename $3=url no file
+	echo $2"(Downloading......)";
+	mkdir $1
+	curl $3$2 > $1/$2  
+		
+}
 
+# Time_Sales_Data/
+#Filename="OptionsDaily_"$(date +"%Y_%m_%d")".zip"
+#url="https://www.taifex.com.tw/file/taifex/Dailydownload/OptionsDailydownloadCSV/"$Filename
+#echo $url
+#dir=$SCRIPTPATH/Time_Sales_Data
+#mkdir $dir
+#curl $url > $dir/$Filename
+getFile $SCRIPTPATH/Time_Sales_Data "OptionsDaily_"$(date +"%Y_%m_%d")".zip" "https://www.taifex.com.tw/file/taifex/Dailydownload/OptionsDailydownloadCSV/"
+
+# Daily_Options_Delta/
+#Filename="Delta_"$(date +"%Y%m%d")".csv"
+#url="https://www.taifex.com.tw/file/taifex/Dailydownload/delta/chinese/"$Filename
+#echo $url
+#dir=$SCRIPTPATH/Daily_Options_Delta
+#mkdir $dir
+#curl $url > $dir/$Filename
+getFile $SCRIPTPATH/Daily_Options_Delta "Delta_"$(date +"%Y%m%d")".csv" "https://www.taifex.com.tw/file/taifex/Dailydownload/delta/chinese/"
+
+# Liquidity_Indicator/
+#Filename="LI_"$(date +"%Y%m%d")".csv"
+#url="https://www.taifex.com.tw/file/taifex/Dailydownload/LI/eng/OPT/"$Filename
+#echo $url
+#dir=$SCRIPTPATH/Liquidity_Indicator
+#mkdir $dir
+#curl $url > $dir/$Filename
+getFile $SCRIPTPATH/Liquidity_Indicator "LI_"$(date +"%Y%m%d")".csv" "https://www.taifex.com.tw/file/taifex/Dailydownload/LI/eng/OPT/"
